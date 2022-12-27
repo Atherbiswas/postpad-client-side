d7bed63f9f60cfba7738b9f3e113e1b13053501f
Ather1230 <ather.biswas0057@gmail.com>
2022-12-27 22:15:34 +0600

    banner added

--- src/Pages/Home/Home/Banner/Banner.js
+++ src/Pages/Home/Home/Banner/Banner.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import React from "react";
 
 const Banner = () => {
-    return (
+  return (
+    <div
+      className="hero bg-[url('/src/assests/banner_pic.jpg')] lg:h-96">
+      <div className="hero-overlay bg-opacity-60"></div>
+      <div className="hero-content text-neutral-content">
         <div>
-            
+          <h1 className="mb-5 lg:text-7xl text-3xl font-bold">Welcome to community</h1>
+          <h1 className="mb-5 lg:text-5xl text-2xl font-bold">Connect, <br /> Share &  Engage</h1>
+          <button className="btn btn-active btn-secondary btn-sm">Learn More</button>
+          <button className="btn btn-active btn-secondary btn-sm mx-3">See All Blog</button>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Banner;
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,14 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'hero-pattern': "url('/src/assests/banner.jpg')",
+        'hero-image': "url('/src/assests/banner_pic.jpg')",
+        'hero-img': "url('/src/assests/header.jpg')",
+        
+      }
+    },
   },
   plugins: [require("daisyui")],
 }
