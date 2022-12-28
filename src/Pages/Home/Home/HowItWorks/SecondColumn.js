1c5d106000eb6aed97127102b43e1d39fa946a19
Ather1230 <ather.biswas0057@gmail.com>
2022-12-28 11:03:54 +0600

    added footer on homepage

--- src/Pages/Home/Home/HowItWorks/SecondColumn.js
+++ src/Pages/Home/Home/HowItWorks/SecondColumn.js
@@ -7,20 +7,20 @@ const SecondColumn = () => {
     return (
         <>
             <div className='flex justify-center items-center'>
-                <FaEdit className='lg:text-3xl text-xl font-bold'></FaEdit>
-                <h2 className='lg:text-3xl text-xl font-bold lg:mx-1'>Create An Account for log in.</h2>
+                <FaEdit className='lg:text-2xl text-xl font-semibold'></FaEdit>
+                <h2 className='lg:text-2xl text-xl font-semibold lg:mx-1'>Create An Account for log in.</h2>
                 </div>
             <div className='flex justify-center items-center mt-5'>
-                <GrUpload className='lg:text-3xl text-xl font-bold'></GrUpload>
-                <h2 className='lg:text-3xl text-xl font-bold lg:mx-1'>Upload image and write blog.</h2>
+                <GrUpload className='lg:text-2xl text-xl font-semibold'></GrUpload>
+                <h2 className='lg:text-2xl text-xl font-semibold lg:mx-1'>Upload image and write blog.</h2>
             </div>
             <div className='flex justify-center items-center mt-5'>
-                <BsFillSuitHeartFill className='lg:text-3xl text-xl font-bold'></BsFillSuitHeartFill>
-                <h2 className='lg:text-3xl text-xl font-bold lg:mx-2'>React Love/Like on the post.</h2>
+                <BsFillSuitHeartFill className='lg:text-2xl text-xl font-semibold'></BsFillSuitHeartFill>
+                <h2 className='lg:text-2xl text-xl font-semibold lg:mx-2'>React Love/Like on the post.</h2>
             </div>
             <div className='flex justify-center items-center  mt-5'>
-                <FaReadme className='lg:text-3xl text-xl font-bold'></FaReadme>
-                <h2 className='lg:text-3xl text-xl font-bold lg:mx-2'>See & read other post/Blog </h2>
+                <FaReadme className='lg:text-2xl text-xl font-semibold'></FaReadme>
+                <h2 className='lg:text-2xl text-xl font-semibold lg:mx-2'>See & read other post/Blog </h2>
             </div>
         </>
     );
--- src/Pages/Home/Home/Statistics/Statistics.js
+++ src/Pages/Home/Home/Statistics/Statistics.js
@@ -4,19 +4,19 @@ const Statistics = () => {
   return (
     <div className="lg:mt-24 mt-5">
       <div className="stats stats-vertical lg:stats-horizontal container mx-auto  bg-emerald-50">
-        <div className="stat text-center">
-          <div className="stat-title">Post Created daily</div>
-          <div className="stat-value text-secondary">1K+</div>
+        <div className="stat text-center lg:mx-5">
+          <div className="stat-title text-2xl">Post Created</div>
+          <div className="stat-value text-secondary text-4xl">15K+</div>
         </div>
 
         <div className="stat text-center">
-          <div className="stat-title">Member Online Right Now</div>
-          <div className="stat-value text-secondary">2.5k+</div>
+          <div className="stat-title text-2xl">Member Online Right Now</div>
+          <div className="stat-value text-secondary text-4xl">2.5k+</div>
         </div>
 
         <div className="stat text-center">
-          <div className="stat-title">New post In Every Week</div>
-          <div className="stat-value text-secondary">1.5k+</div>
+          <div className="stat-title text-2xl">New post In Every Week</div>
+          <div className="stat-value text-secondary text-4xl">1.5k+</div>
         </div>
       </div>
     </div>
